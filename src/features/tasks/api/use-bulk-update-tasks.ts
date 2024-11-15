fe06508eaa79f289278a8430cb41f2d86a81de6d
ayan471 <pakhiraayan@gmail.com>
2024-11-15 18:56:44 +0530

    fixed bugs

--- src/features/tasks/api/use-bulk-update-tasks.ts
+++ src/features/tasks/api/use-bulk-update-tasks.ts
@@ -25,6 +25,8 @@ export const useBulkUpdateTasks = () => {
     onSuccess: () => {
       toast.success("Tasks updated successfully");
 
+      queryClient.invalidateQueries({ queryKey: ["project-analytics"] });
+      queryClient.invalidateQueries({ queryKey: ["workspace-analytics"] });
       queryClient.invalidateQueries({ queryKey: ["tasks"] });
     },
     onError: () => {
